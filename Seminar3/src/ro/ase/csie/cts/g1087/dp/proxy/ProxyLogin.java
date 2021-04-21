b6e085ef0c9c2d607a075104bf47508a050af548
tatumarian99 <tatumarian99@gmail.com>
2021-04-21 14:45:35 +0300

    proxy

---
+++ Seminar3/src/ro/ase/csie/cts/g1087/dp/proxy/ProxyLogin.java
@@ -0,0 +1,52 @@
+package ro.ase.csie.cts.g1087.dp.proxy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProxyLogin implements InterfataModulLogin{
+
+	InterfataModulLogin modulLogin = null;
+	
+	public static final int NR_MAX_INCERCARI = 3;
+	public Map<String, Integer> nrIncercari = new HashMap<>();
+	
+	public ProxyLogin(InterfataModulLogin modulLogin) {
+		super();
+		this.modulLogin = modulLogin;
+	}
+
+	@Override
+	public boolean login(String username, String pass) {
+		Integer nrIncercariNereusite = nrIncercari.get(username);//daca nu exista aceasta cheie in map primim null
+		if(nrIncercariNereusite == null) { //e prima data cand cineva incearca sa se logheze
+			nrIncercari.put(username, 0);
+			nrIncercariNereusite = 0;
+		}
+		if(modulLogin != null) {
+			if(nrIncercariNereusite < NR_MAX_INCERCARI) {//deja cand ajungi la 2, faci a 3a incercare
+				if(modulLogin.login(username, pass) == true) {
+					nrIncercariNereusite = 0;
+					return true;
+				}
+				else {
+					nrIncercariNereusite += 1;
+					nrIncercari.put(username, nrIncercariNereusite);
+					return false;
+				}
+			} else
+				return false;
+		} else
+			throw new UnsupportedOperationException();
+	}
+	
+
+	@Override
+	public boolean verificaStatusServer() {
+		if ( modulLogin != null ) {
+			return modulLogin.verificaStatusServer();
+		} else {
+			throw new UnsupportedOperationException();
+		}
+	}
+
+}
